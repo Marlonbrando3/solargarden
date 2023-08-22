fdd4bcfe0de8f4530739f8c6ed87d13363b38830
Marek <marek@MacBook-Air-Marek.local>
2023-08-22 16:32:34 +0200

    first commit

--- src/Header/header.tsx
+++ src/Header/header.tsx
@@ -15,6 +15,12 @@ export default function Header() {
         setMenuShowed(!menuShowed)
     }
 
+    const handleClikedMenuOnMobile =() => {
+        if(menuShowed === true){
+            setMenuShowed(false)
+        }
+    }
+
 
   return (
     <div className='h-[100px] w-screen flex justify-center'>
@@ -24,14 +30,14 @@ export default function Header() {
                 <div className='w-[130px] xl:w-[180px] h-[100px] relative md:mr-[40px]'>
                     <Image alt="logo" src="/Green_logo_solar_garden.png" fill className='object-contain py-[12px]'></Image>
                 </div>
-            <div data-ref={menu} className={menuShowed === true ? 'flex text-[16px] bg-white items-center h-screen xl:h-[100px] xl:w-[500px] w-[100vw] flex-col xl:flex-row absolute xl:static -right-[0%] duration-150 z-50' : 
+            <div data-ref={menu} className={menuShowed === true ? 'flex text-[16px] bg-white items-center h-screen xl:h-[100px] xl:w-[500px] w-[100vw] flex-col xl:flex-row justify-center absolute xl:static -right-[0%] duration-150 z-50' : 
             'flex text-[16px] bg-white items-center w-[100vw] h-[100vh] xl:h-[100px] xl:w-[860px] flex-col xl:flex-row absolute xl:static -right-[100vw] duration-150 z-50'}>
-                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]'>Strona główna</p>
-                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]'>Dla domu</p>
-                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]'>Dla firmy</p>
-                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]'>Dla rolnictwa</p>
-                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]'>Baza wiedzy</p>
-                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]'>Kontakt</p>
+                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]' onClick={handleClikedMenuOnMobile}>Strona główna</p>
+                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]' onClick={handleClikedMenuOnMobile}>Dla domu</p>
+                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]' onClick={handleClikedMenuOnMobile}>Dla firmy</p>
+                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]' onClick={handleClikedMenuOnMobile}>Dla rolnictwa</p>
+                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]' onClick={handleClikedMenuOnMobile}>Baza wiedzy</p>
+                <p className='px-[11px] cursor-pointer duration-300 border-b-white border-b-2 hover:border-b-[#a7e06a]' onClick={handleClikedMenuOnMobile}>Kontakt</p>
             </div>
             <div className='xl:ml-[40px]  w-[200] md:w-[300px]'>
                 <div className='text-[10px] md:text-[16px] flex flex-col justify-center items-end h-full pl-[30px] md:text-white'>
