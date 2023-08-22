f29f275f530e2082a3bc19cda73b3573f819b83b
Marek <marek@MacBook-Air-Marek.local>
2023-08-22 15:23:23 +0200

    first commit

--- src/HomeView/button.tsx
+++ src/HomeView/button.tsx
@@ -1,22 +1,54 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRef } from 'react'
 import {MdOutlineSolarPower} from 'react-icons/md'
 import {TbWindmill} from 'react-icons/tb'
 import {MdOutlineStorage} from 'react-icons/md'
 
 export default function Button() {
+
+
+    const photovoltaic = useRef();
+    const heatPump = useRef();
+    const energyBank = useRef();
+
+
+      useEffect(() => {
+        setTimeout(() => {
+
+            photovoltaic.current.style.backgroundColor = "#DFFFD2"
+    
+          }, 1000);
+    
+          setTimeout(() => {
+    
+            heatPump.current.style.backgroundColor = "#DFFFD2"
+    
+          }, 2000);
+    
+          setTimeout(() => {
+    
+            energyBank.current.style.backgroundColor = "#DFFFD2"
+    
+          }, 3000);
+
+
+      },[])
+
+
+
   return (
     <div className='w-screen h-[11vh] md:h-[100px] flex justify-center'>
         <div className='w-[90vw] md:w-[1150px] md:h-[100px] flex justify-start items-center'>
             <div className='hidden md:flex items-center'><p className='md:text-[15px] md:w-[210px] leading-6'>Wybierz usługę dla siebie:</p></div>
-            <div className='bg-green-500/[0.1] rounded-[10px] w-full md:w-[200px] h-[60px] md:h-[70px] border-red-900 flex flex-col md:flex-row justify-center items-center md:text-[15px] leading-6 md:border-l mx-[2px] md:mx-auto'>
+            <div ref={photovoltaic} className='bg-transparent rounded-[10px] w-full md:w-[200px] h-[60px] md:h-[70px] border-red-900 flex flex-col md:flex-row justify-center items-center md:text-[15px] leading-6 md:border-l mx-[2px] md:mx-auto transition-colors'>
                 <MdOutlineSolarPower className='text-[60px] md:text-[60px] mx-[10px]'/>
                 <p className='text-center text-[12px] leading-4'>Instalacje Fotowoltaiczne</p>
             </div>
-            <div className='bg-green-500/[0.1] rounded-[10px] w-full md:w-[200px] h-[60px] md:h-[70px] border-red-900 flex flex-col md:flex-row justify-center items-center md:text-[15px] leading-6 md:border- mx-[2px] md:mx-autol'>
+            <div ref={heatPump} className='bg-transparent rounded-[10px] w-full md:w-[200px] h-[60px] md:h-[70px] border-red-900 flex flex-col md:flex-row justify-center items-center md:text-[15px] leading-6 md:border- mx-[2px] md:mx-auto transition-colors'>
                 <TbWindmill className='text-[40px] md:text-[60px]  mx-[10px]'/>
                 <p className='text-center text-[12px] leading-4'>Pompy <br></br>Ciepła</p>
             </div>
-            <div className='bg-green-500/[0.1] rounded-[10px] w-full md:w-[200px] h-[60px] md:h-[70px] border-red-900 flex flex-col md:flex-row justify-center items-center md:text-[15px] leading-6 md:border-l mx-[2px] md:mx-auto'>
+            <div ref={energyBank} className='bg-transparent rounded-[10px] w-full md:w-[200px] h-[60px] md:h-[70px] border-red-900 flex flex-col md:flex-row justify-center items-center md:text-[15px] leading-6 md:border-l mx-[2px] md:mx-auto transition-colors'>
                 <MdOutlineStorage className='text-[40px] md:text-[60px] mx-[10px]'/>
                 <p className='text-center text-[12px] leading-4'>Magazyny <br></br>Energii</p>
             </div>
